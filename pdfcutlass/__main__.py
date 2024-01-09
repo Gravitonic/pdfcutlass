0d4ace4b110174e05deffe18c43d47a8805e5acf
Gravitonic <dylany360@gmail.com>
2024-01-09 13:56:03 -0800

    Add basic splitting functionality

--- pdfcutlass/__main__.py
+++ pdfcutlass/__main__.py
@@ -3,7 +3,8 @@
 import argparse
 import json
 import sys
-from pypdf import PdfReader, PdfWriter
+from pathlib import Path
+from pypdf import PdfReader, PdfWriter, PageRange
 
 
 def load_parser():
@@ -14,7 +15,8 @@ def load_parser():
     )
     parser.add_argument("input_file")
     parser.add_argument("output_file")
-    parser.add_argument("--outline")
+    parser.add_argument("--outline", required=True)
+    parser.add_argument("--split", action="store_true")
     return parser
 
 
@@ -27,19 +29,64 @@ def main():
     reader = PdfReader(args.input_file)
 
     writer = PdfWriter()
-    writer.append(reader, import_outline=False)
 
     with open(args.outline, encoding="utf-8") as f:
         new_outline = json.load(f)
 
     current_parent = None
 
-    for bookmark in new_outline:
+    if args.split:
+        Path.mkdir(args.output_file, exist_ok=True)
+
+    for bookmark, next_bookmark in zip(new_outline, new_outline[1:] + [None]):
         if "level" not in bookmark or bookmark["level"] == 1:
             current_parent = writer.add_outline_item(
                 title=bookmark["title"],
                 page_number=bookmark["page_number"] - 1,
             )
+
+            if args.split:
+                section_writer = PdfWriter()
+
+                page_range = PageRange(
+                    slice(
+                        bookmark["page_number"] - 1,
+                        next_bookmark["page_number"] - 1
+                        if next_bookmark is not None
+                        else None,
+                    )
+                )
+
+                section_writer.append(
+                    reader,
+                    pages=page_range,
+                    import_outline=False,
+                )
+
+                # section_start = bookmark["page_number"] - 1
+                # section_end = (
+                #     next_bookmark["page_number"] - 1
+                #     if next_bookmark is not None
+                #     else None
+                # )
+                # section_pages = reader.pages[section_start:section_end]
+
+                # section_writer.append
+
+                # for page in section_pages:
+                #     section_writer.add_page(page)
+
+                # # section_pages =
+
+                # for page in reader.pages[
+                #     bookmark["page_number"] : next_bookmark["page_number"]
+                # ]:
+                #     section_writer.add_page(page)
+
+                with open(
+                    Path(args.output_file) / f"{bookmark['title']}.pdf", "wb"
+                ) as outfile:
+                    section_writer.write(outfile)
         elif current_parent is None:
             print("Error: no parent for child bookmark.", file=sys.stderr)
             sys.exit(1)
@@ -50,8 +97,10 @@ def main():
                 parent=current_parent,
             )
 
-    with open(args.output_file, "wb") as outfile:
-        writer.write(outfile)
+    if not args.split:
+        writer.append(reader, import_outline=False)
+        with open(args.output_file, "wb") as outfile:
+            writer.write(outfile)
 
 
 if __name__ == "__main__":
